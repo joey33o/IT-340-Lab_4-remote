3e03b18a87a848bca4c799217a44b82ff9246f81
Joseph <jm2569@njit.edu>
2026-02-22 17:51:04 -0500

    Modified: monitor.sh file to echo DAILY MEMORY CHECK instead of Memory log

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Log the date and memory usage 
 
-echo "Memory Log - $(date)" >> /home/joey/Lab_4/system_log.txt
+echo "DAILY MEMORY CHECK - $(date)" >> /home/joey/Lab_4/system_log.txt
 free -h | grep Mem >> /home/joey/Lab_4/system_log.txt
 echo "--------------------------------" >> /home/joey/Lab_4/system_log.txt
 
