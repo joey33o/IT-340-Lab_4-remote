db0c00c138ad0b339e4f9201e8252808ade8ae12
Joseph <jm2569@njit.edu>
2026-02-22 17:45:11 -0500

    Fixed monitor.sh to append to system_log.txt at correct abs path

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Log the date and memory usage 
 
-echo "Memory Log - $(date)" >> system_log.txt
-free -h | grep Mem >> system_log.txt
-echo "--------------------------------" >> system_log.txt
+echo "Memory Log - $(date)" >> /home/joey/Lab_4/system_log.txt
+free -h | grep Mem >> /home/joey/Lab_4/system_log.txt
+echo "--------------------------------" >> /home/joey/Lab_4/system_log.txt
+
